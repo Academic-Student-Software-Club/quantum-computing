288765810e107d0da28ff6e9d78bed6b49b485e5
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-11 17:39:05 -0700

    Update 7-triple-pauli.py

--- quantum-circuits/7-triple-pauli.py
+++ quantum-circuits/7-triple-pauli.py
@@ -1,6 +1,18 @@
 import numpy as np
+import sympy
+from sympy import symbols
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Operator, Statevector
 from qiskit.visualization import plot_state_city
 import matplotlib.pyplot as plt
+
+X = np.array([[0,1],[1,0]])
+Y = np.array([[0,-1j],[1j,0]])
+Z = np.array([[1,0],[0,-1]])
+
+a, b, c = symbols('a b c')
+
+U = np.exp(a)
+print(U)
+
 
