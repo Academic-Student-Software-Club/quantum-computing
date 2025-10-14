5edb2c5aec0de81d38edd87df22dfca3952d107b
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-13 20:34:00 -0700

    Update 1-introduction.py

--- quantum-fourier-transform/1-introduction.py
+++ quantum-fourier-transform/1-introduction.py
@@ -4,6 +4,15 @@ from qiskit.tools.monitor import job_monitor
 from qiskit.circuit.library import QFT
 import numpy as np
 
+
+########################
+# I dont actually know how this worked.
+# I copied this example just to try it out,
+# but I need to use my IBM minutes
+# and I dont wanna.
+########################
+
+
 pi = np.pi
 
 IBMQ.enable_account("ENTER API KEY HERE")
