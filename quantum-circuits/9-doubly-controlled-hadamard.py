0ff2e8e7eebf6d420419cad2492eb4359e6e6dc5
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-23 23:08:01 -0700

    Update 9-doubly-controlled-hadamard.py

--- quantum-circuits/9-doubly-controlled-hadamard.py
+++ quantum-circuits/9-doubly-controlled-hadamard.py
@@ -6,6 +6,7 @@ from qiskit.quantum_info import Operator, Statevector
 from qiskit.visualization import plot_state_city
 
 from qiskit.circuit.library import HGate
+import cmath
 
 
 # Decompose the doubly controlled Hadamard gate below in terms of
@@ -31,7 +32,27 @@ a = symbols('a')
 
 H = (1/np.sqrt(2))*np.array([[1,1],[1,-1]])
 e_val,P = np.linalg.eig(H)
+P2 = np.array([[1+np.sqrt(2),1],[1-np.sqrt(2),1]])
 D=np.array([[e_val[0],0],[0,e_val[1]]])
 P_inv = np.linalg.inv(P)
-print(P@D@P_inv)
+P2_inv = np.linalg.inv(P2)
+print(P2@D@P2_inv)
+print("----------------")
+
+# Create V and V_dagger.
+# They are defined as P (sqrt(D)) P^-1
+
+D2=np.array([[cmath.sqrt(e_val[0]),0],[0,np.round(cmath.sqrt(e_val[1]))]])
+V = P@D2@P_inv
+V_dagger = V.conj().T
+print(V)
+print(V_dagger)
+print("--------------")
+print(V@V_dagger)
+print(np.round(V@V_dagger))
+
+print("-------------------")
+
+
+
 
