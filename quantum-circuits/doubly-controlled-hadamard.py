98852f15732c5557f93947a968461d06fabf60ac
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-23 20:45:15 -0700

    Create doubly-controlled-hadamard.py

---
+++ quantum-circuits/doubly-controlled-hadamard.py
@@ -0,0 +1,7 @@
+import numpy as np
+import sympy
+from sympy import symbols
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator, Statevector
+from qiskit.visualization import plot_state_city
+import matplotlib.pyplot as plt
