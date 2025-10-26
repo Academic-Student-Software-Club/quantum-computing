1874a444938988923c4cbe5fed73f8eb7214d1a5
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-25 18:04:47 -0700

    Update 10-HW4.py

--- quantum-circuits/10-HW4.py
+++ quantum-circuits/10-HW4.py
@@ -21,5 +21,23 @@ Dhalf = np.array([[cmath.sqrt(e_val[0]),0],[0,cmath.sqrt(e_val[1])]])
 K = e_vec
 Kinv = np.linalg.inv(K)
 
-# Now we make V, using K * Dhalf * Kinv.
+# Now we make V and Vdag, using K * Dhalf * Kinv.
+
+V = K@Dhalf@Kinv
+Vdag = V.T.conj()
+
+# And now we construct the circuit.
+
+qc = QuantumCircuit(3)
+
+# Controlled V on bits [1,2]
+qc.unitary(Operator(V),[2],label="V")
+qc.barrier()
+# CNOT on [0,1]
+qc.cx(0,1)
+qc.barrier()
+
+# Controlled Vdagger on bits [1,2] 
+
+print(qc)
 
