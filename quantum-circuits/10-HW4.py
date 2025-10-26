7fdd4cc3cd60163c3297c8cedba62b010d76f861
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-25 17:55:00 -0700

    Create 10-HW4.py

---
+++ quantum-circuits/10-HW4.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+from sympy import symbols
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator, Statevector
+from qiskit.visualization import plot_state_city
+
+from qiskit.circuit.library import HGate
+import cmath
+
+H = (1/np.sqrt(2))*np.array([[1,1],[1,-1]])
+
+# Gets eigenvalues/vectors of H.
+e_val,e_vec = np.linalg.eig(H)
+
+# Constructs D and D^{1.2}
+D = np.array([[e_val[0],0],[0,e_val[1]]])
+Dhalf = np.array([[cmath.sqrt(e_val[0]),0],[0,cmath.sqrt(e_val[1])]])
+
+# Construct K and K^-1
+K = e_vec
+Kinv = np.linalg.inv(K)
+
+# Now we make V, using K * Dhalf * Kinv.
+
