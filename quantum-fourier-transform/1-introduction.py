a97d7a4c1280045564aaf75f9b042fe4b58f1cde
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-10-27 21:10:55 -0700

    Update 1-introduction.py

--- quantum-fourier-transform/1-introduction.py
+++ quantum-fourier-transform/1-introduction.py
@@ -1,9 +1,12 @@
 from qiskit import QuantumRegister, ClassicalRegister
-from qiskit import QuantumCircuit, execute,IBMQ
-from qiskit.tools.monitor import job_monitor
+from qiskit import QuantumCircuit
 from qiskit.circuit.library import QFT
 import numpy as np
-
+np.set_printoptions(suppress=True,formatter={'all': lambda x: "{:.4g}".format(x)})
+from sympy import symbols
+from qiskit.circuit.library import UnitaryGate
+from qiskit.quantum_info import Operator, Statevector
+from qiskit.visualization import plot_state_city
 
 ########################
 # I dont actually know how this worked.
@@ -12,4 +15,35 @@ import numpy as np
 # and I dont wanna.
 ########################
 
+initial_states = ["00",'01','10','11']
+U = np.zeros((len(initial_states),len(initial_states)),dtype=complex)
+n = 0
+m = 0
 
+while n < len(initial_states):
+    m=0
+    while m < len(initial_states):
+        U[n,m]= U[n,m]+np.e**(2j * np.pi *n*m/len(initial_states))
+        m = m+1
+    n = n+1
+
+threshold = 1e-10
+U.real[np.abs(U.real)<threshold]=0
+U.imag[np.abs(U.imag)<threshold]=0
+
+print(U)
+
+
+
+
+if 1==2:
+    for state in initial_states:
+        sv = Statevector.from_label(state)
+        arr = np.array(sv)
+        col = arr.reshape(-1,1)
+        print("col:")
+        print(col)
+        U2 = np.append(U2,col,axis=1)
+    print("total U")
+    U2 = U2[:,1:]
+    print(U2)
