4bd2320af8a8955fb280dcad413d1a35aaa6a4c3
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2025-12-05 10:54:39 -0800

    Update 4-HW8.py

--- misc/4-HW8.py
+++ misc/4-HW8.py
@@ -11,15 +11,25 @@ t = int(input("Input time t:"))
 qc = QuantumCircuit(N)
 
 m=1
-while m < M:
-    j = 0
-    while j<=(N-2):
-        qc.rzz(2*(t/M),j,j+1)
-        j+=1
+while m <= M:
     j = 0
     while j<=(N-1):
         qc.rx(2*(t/M)*B,j)
         j+=1
+    j = 0
+    while j<=(N-2):
+        qc.rzz(2*(t/M),j,j+1)
+        j+=1
     m+=1
+    qc.barrier()
 
-print(qc)
+psi_in = Statevector.from_instruction(qc)
+
+psi_out = psi_in.evolve(qc.reverse_bits())
+psi_out_dict = psi_out.to_dict()
+print(" ")
+print("Evolution of |0>")
+for basis, amplitude in psi_out_dict.items():
+    print(f"|{basis}>: {amplitude}")
+
+qc.draw('mpl')
